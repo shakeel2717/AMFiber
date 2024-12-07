8be1bbb81a8b8c1f0f8c386ccd9f0e99ba8e368f
nisha783 <hm6980097@gmail.com>
2024-12-07 16:50:57 +0500

    123

--- app/Livewire/QuotationCreate.php
+++ app/Livewire/QuotationCreate.php
@@ -26,6 +26,36 @@ class QuotationCreate extends Component
 
     public function mount()
     {
+
+        $this->customers = Party::where('type', 'customer')->get();
+        $this->products = \App\Models\Product::get();
+        $this->plais = \App\Models\Plai::get();
+    }
+
+
+    
+    public function updatedCustomerSearch()
+    {
+        if (!empty($this->customerSearch)) {
+            // Perform server-side search
+            $this->customers = Party::where('type', 'customer')
+                ->when($this->customerSearch, function ($query) {
+                    return $query->where(function ($q) {
+                        $q->where('name', 'like', '%' . $this->customerSearch . '%')
+                            ->orWhere('phone', 'like', '%' . $this->customerSearch . '%');
+                    });
+                })
+                ->limit(50) // Limit results to prevent overwhelming the select
+                ->get();
+        } else {
+            $this->customers = Party::where('type', 'customer')->get();
+        }
+    }
+
+    // Method to clear search and reset customers
+    public function clearCustomerSearch()
+    {
+        $this->customerSearch = '';
         $this->customers = Party::where('type', 'customer')->get();
     }
 
