b10fd21d6d6afbb769d2415b09e0cfb4f6b34560
Shakeel Ahmad <shakeel2717@gmail.com>
2024-12-07 23:17:30 +0500

    revert all nisha chaneges to default

--- resources/views/livewire/invoice-create.blade.php
+++ resources/views/livewire/invoice-create.blade.php
@@ -7,36 +7,23 @@
                 <i class="fa fa-spinner fa-spin"></i>
             </span>
             <div class="input-group mb-3">
-                <input 
-                    type="text" 
-                    class="form-control" 
-                    placeholder="Search by name, email, or phone" 
-                    wire:model.live.debounce.500ms="customerSearch"
-                >
+                <input type="text" class="form-control" placeholder="Search by name, email, or phone"
+                    wire:model.live.debounce.500ms="customerSearch">
                 <div class="input-group-append">
-                    <button 
-                        class="btn btn-outline-secondary" 
-                        type="button" 
-                        wire:click="clearCustomerSearch"
-                    >
+                    <button class="btn btn-outline-secondary" type="button" wire:click="clearCustomerSearch">
                         <i class="fa fa-times"></i>
                     </button>
                 </div>
             </div>
 
             <label for="customer_id">Select Customer</label>
-            <select 
-                wire:model.live="selectedCustomer" 
-                name="customer_id" 
-                id="customer_id" 
-                class="js-select2 form-control"
-                wire:key="select-customer-{{ now() }}"
-            >
+            <select wire:model.live="selectedCustomer" name="customer_id" id="customer_id"
+                class="js-select2 form-control" wire:key="select-customer-{{ now() }}">
                 <option value="">Select a Customer</option>
                 @foreach ($customers as $customer)
                     <option value="{{ $customer->id }}">
-                        {{ $customer->name }} 
-                        @if($customer->email)
+                        {{ $customer->name }}
+                        @if ($customer->email)
                             ({{ $customer->email }})
                         @endif
                     </option>
