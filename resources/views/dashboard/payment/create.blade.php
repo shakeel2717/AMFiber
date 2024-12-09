e31d0ad3f49cd51ae4a87f05179d53b2ca329e74
Shakeel Ahmad <shakeel2717@gmail.com>
2024-12-09 15:55:10 +0500

    Payment Bug Fixed

--- resources/views/dashboard/payment/create.blade.php
+++ resources/views/dashboard/payment/create.blade.php
@@ -7,7 +7,36 @@
         <div class="col-md-8 mx-auto">
             <div class="card">
                 <div class="card-body">
-                   @livewire('payment-create')
+                    <form action="{{ route('payment.store') }}" method="POST">
+                        @csrf
+                        @livewire('payment-create')
+                        <div class="form-group">
+                            <label for="amount">Amount</label>
+                            <input type="text" name="amount" id="amount" class="form-control"
+                                placeholder="Enter Amount you Received">
+                        </div>
+                        <div class="form-group">
+                            <label for="reduction">Reduction</label>
+                            <input type="text" name="reduction" id="reduction" class="form-control"
+                                placeholder="Enter extra discount for customer">
+                        </div>
+                        <div class="form-group">
+                            <label for="payment_method">Payment Method</label>
+                            <select name="payment_method" id="payment_method" class="js-select2 form-control form-select">
+                                <option value="Cash" selected>Cash</option>
+                                <option value="Bank">Bank</option>
+                                <option value="Mobicash">Mobicash</option>
+                                <option value="EasyPaisa">EasyPaisa</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="reference">Reference / Note</label>
+                            <textarea name="reference" id="reference" cols="30" rows="2" class="form-control"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary" value="Add Payment">
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
