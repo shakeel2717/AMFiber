b556719c6bed5a7ac33349d05321d7e2dad62d36
Shakeel Ahmad <shakeel2717@gmail.com>
2025-09-28 21:55:46 +0500

    font size update

--- resources/views/inc/logo.blade.php
+++ resources/views/inc/logo.blade.php
@@ -3,7 +3,7 @@
         <img src="{{ asset('logo.png') }}" alt="Logo" class="thermal-logo">
     </div>
     <div class="company-name">
-        <h1>Al-Mukhtar Fiber Glass House</h1>
+        <h1 style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif">Al-Mukhtar Fiber Glass House</h1>
     </div>
     <div class="thermal-line"></div>
 </div>
